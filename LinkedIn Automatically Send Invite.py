20badf0b8a8968350c95abb39450299e6edbe4d6
Yiliang Liu <37753789+yiliang-Liu@users.noreply.github.com>
2018-06-11 01:04:05 -0400

    Add files via upload

---
+++ LinkedIn Automatically Send Invite.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 11 01:01:25 2018
+
+@author: yl_33
+"""
+
+from selenium import webdriver
+
+# Change the information in the below dictionary
+Info = {
+    # Your account info
+    'username': 'XXX',
+    'password': 'XXX',
+    
+    # the homepage link of the person you want to add 
+    'target_link': 'https://www.linkedin.com/in/XXX',
+}
+
+# The invitation text you want to send, No more than 300 words
+invite_text = "XXX"
+
+
+def LinkedIn_Add_Connect(Info, invite_text):
+    """
+    func: automatically add people and send invitation text
+    """
+    # the path of chromedriver
+    chromedriver = 'path'
+    
+    # Open LinkedIn 
+    url = 'https://www.linkedin.com'
+    browser = webdriver.Chrome(chromedriver)
+    browser.get(url)
+    
+    # Sing in LinkedIn
+    username_field = browser.find_element_by_id('login-email')
+    password_field = browser.find_element_by_id("login-password")
+    username = Info.get('username')
+    password = Info.get('password')
+    username_field.send_keys(username)
+    browser.implicitly_wait(1)
+    password_field.send_keys(password)
+    browser.implicitly_wait(1)
+    # Click for sign in 
+    browser.find_element_by_id('login-submit').click()
+    # Check tab title
+    print("Tag title: " + browser.title)
+
+    # Open new page
+    browser.execute_script("window.open('{}', 'new_window')".format(Info.get('target_link')))
+    
+    # Switch to the new page
+    # https://stackoverflow.com/questions/37088589/selenium-wont-open-a-new-url-in-a-new-tab-python-chrome
+    browser.switch_to.window(browser.window_handles[1])
+    # Check title
+    print("Tag title: " + browser.title)
+    
+    # Click Connect
+    browser.find_element_by_class_name('pv-s-profile-actions').click()
+    
+    # Click add note
+    browser.find_element_by_class_name('send-invite__actions').click()
+    
+    invite_field = browser.find_element_by_id('custom-message')
+    # Write message
+    invite_field.send_keys(invite_text)
+    # Click send message button
+    browser.find_element_by_css_selector('.button-primary-large.ml1').click()
+    # Quick 
+    browser.quit()
+    print('Success!')
+    
+if __name__ == "__main__":
+    LinkedIn_Add_Connect(Info, invite_text)
